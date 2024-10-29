ad02ad2bba5f3eced751fd4a9353921774b0535f
LOHAN2000 <anghelovy@outlook.com>
2024-10-29 14:12:42 -0500

    added followers and followed

--- app/models.py
+++ app/models.py
@@ -7,6 +7,13 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db, login
 
+followers = sa.Table(
+    "followers",
+    db.metadata, 
+    sa.Column("follower_id", sa.Integer, sa.ForeignKey("user.id"), primary_key=True),
+    sa.Column("followed_id", sa.Integer, sa.ForeignKey("user.id"), primary_key=True)
+)
+
 class User(db.Model, UserMixin):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
@@ -15,6 +22,9 @@ class User(db.Model, UserMixin):
     about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
     last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(default=lambda: datetime.now(timezone.utc))
     posts: so.WriteOnlyMapped["Post"] = so.relationship(back_populates="author") #Crea la relacion con la otra tabla mediante un valor 
+
+    following: so.WriteOnlyMapped["User"] = so.relationship(secondary=followers, primaryjoin=(followers.c.follower_id == id), secondaryjoin=(followers.c.followed_id == id), back_populates="followers")
+    followers: so.WriteOnlyMapped["User"] = so.relationship(secondary=followers, primaryjoin=(followers.c.followed_id == id), secondaryjoin=(followers.c.follower_id == id), back_populates="following")
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -25,6 +35,31 @@ class User(db.Model, UserMixin):
     def avatar(self, size):
         digest = md5(self.email.lower().encode("utf-8")).hexdigest()
         return f"https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}"
+    
+    def follow(self, user):
+        if not self.is_following(user):
+            self.following.add(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.following.remove(user)
+    
+    def is_following(self, user):
+        query = self.following.select().where(User.id == user.id)
+        return db.session.scalar(query) is not None
+    
+    def followers_count(self):
+        query = sa.select(sa.func.count()).select_from(self.followers.select().subquery())
+        return db.session.scalar(query)
+    
+    def following_count(self):
+        query = sa.select(sa.func.count()).select_from(self.following.select().subquery())
+        return db.session.scalar(query)
+    
+    def following_posts(self):
+        Author = so.aliased(User)
+        Follower = so.aliased(User)
+        return (sa.select(Post).join(Post.author.of_type(Author)).join(Author.followers.of_type(Follower), isouter=True).where(sa.or_(Follower.id == self.id, Author.id == self.id)).group_by(Post).order_by(Post.timestamp.desc()))
 
     def __repr__(self):
         return "<User {}>".format(self.username)
@@ -39,7 +74,6 @@ class Post(db.Model):
     timestamp: so.Mapped[datetime] = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
     user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True) #Crea la referencia a la tabla
     author: so.Mapped[User] = so.relationship(back_populates="posts") #Crea la relacion con la otra tabla mediante un valor 
-
 
     def __repr__(self):
         return "<Post {}>".format(self.body)
